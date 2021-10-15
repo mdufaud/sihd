05f6ec834a6fe6c062b66efb8358d71da1c26610
mdufaud <maxence_dufaud@hotmail.fr>
2021-10-15 15:19:48 +0200

    Fixed templates

--- _build_tools/makefile/templates.mk
+++ _build_tools/makefile/templates.mk
@@ -1,11 +1,11 @@
-BUILD_TOOLS_TEMPLATES = $(BUILD_TOOLS)/templates
-BUILD_TOOLS_SCRIPTS = $(BUILD_TOOLS)/scripts
+MAKEFILE_TOOLS_TEMPLATES = $(MAKEFILE_TOOLS)/templates
+MAKEFILE_TOOLS_SCRIPTS = $(MAKEFILE_TOOLS)/scripts
 
 ifeq ($(word 1, $(MAKECMDGOALS)), newmod)
 MODULE_NAME=$(word 2, $(MAKECMDGOALS))$(m)
 
 newmod:
-	@bash $(BUILD_TOOLS_SCRIPTS)/make_module.sh $(APP_NAME) $(MODULE_NAME)
+	@bash $(MAKEFILE_TOOLS_SCRIPTS)/make_module.sh $(APP_NAME) $(MODULE_NAME)
 
 # for no 'no rules to make...'
 $(MODULE_NAME):
@@ -17,7 +17,7 @@ MODULE_NAME=$(word 2, $(MAKECMDGOALS))$(m)
 TEST_NAME=$(word 3, $(MAKECMDGOALS))$(t)
 
 newtest:
-	@bash $(BUILD_TOOLS_SCRIPTS)/make_test.sh $(APP_NAME) $(MODULE_NAME) $(TEST_NAME)
+	@bash $(MAKEFILE_TOOLS_SCRIPTS)/make_test.sh $(APP_NAME) $(MODULE_NAME) $(TEST_NAME)
 
 # for no 'no rules to make...'
 $(MODULE_NAME):
@@ -33,7 +33,7 @@ MODULE_NAME=$(word 2, $(MAKECMDGOALS))$(m)
 CLASS_NAME=$(word 3, $(MAKECMDGOALS))$(c)
 
 newclass:
-	@bash $(BUILD_TOOLS_SCRIPTS)/make_class.sh $(APP_NAME) $(MODULE_NAME) $(CLASS_NAME)
+	@bash $(MAKEFILE_TOOLS_SCRIPTS)/make_class.sh $(APP_NAME) $(MODULE_NAME) $(CLASS_NAME)
 
 # for no 'no rules to make...'
 $(MODULE_NAME):
@@ -48,7 +48,7 @@ MODULE_NAME=$(word 2, $(MAKECMDGOALS))$(m)
 CLASS_NAME=$(word 3, $(MAKECMDGOALS))$(c)
 
 newnamedclass:
-	@bash $(BUILD_TOOLS_SCRIPTS)/make_named_class.sh $(APP_NAME) $(MODULE_NAME) $(CLASS_NAME)
+	@bash $(MAKEFILE_TOOLS_SCRIPTS)/make_named_class.sh $(APP_NAME) $(MODULE_NAME) $(CLASS_NAME)
 
 # for no 'no rules to make...'
 $(MODULE_NAME):
@@ -63,7 +63,7 @@ MODULE_NAME=$(word 2, $(MAKECMDGOALS))$(m)
 CLASS_NAME=$(word 3, $(MAKECMDGOALS))$(c)
 
 newinterface:
-	@bash $(BUILD_TOOLS_SCRIPTS)/make_interface_file.sh $(APP_NAME) $(MODULE_NAME) $(CLASS_NAME)
+	@bash $(MAKEFILE_TOOLS_SCRIPTS)/make_interface_file.sh $(APP_NAME) $(MODULE_NAME) $(CLASS_NAME)
 
 # for no 'no rules to make...'
 $(MODULE_NAME):
@@ -78,7 +78,7 @@ MODULE_NAME=$(word 2, $(MAKECMDGOALS))$(m)
 CLASS_NAME=$(word 3, $(MAKECMDGOALS))$(c)
 
 newdevice:
-	@bash $(BUILD_TOOLS_SCRIPTS)/make_device_class.sh $(APP_NAME) $(MODULE_NAME) $(CLASS_NAME)
+	@bash $(MAKEFILE_TOOLS_SCRIPTS)/make_device_class.sh $(APP_NAME) $(MODULE_NAME) $(CLASS_NAME)
 
 # for no 'no rules to make...'
 $(MODULE_NAME):
