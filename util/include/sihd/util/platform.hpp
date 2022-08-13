4809a362557fe1df9ec895c170165d6d8174a29f
mdufaud <maxence_dufaud@hotmail.fr>
2022-08-13 02:38:45 +0200

    up

--- util/include/sihd/util/platform.hpp
+++ util/include/sihd/util/platform.hpp
@@ -6,14 +6,15 @@
 #  define __SIHD_EMSCRIPTEN__
 # endif
 
-# if ((defined(_WIN16) || defined(_WIN32) || defined(_WIN64) || defined(__WINDOWS__) || defined(__MINGW32__)) \
-        && !defined(__MSYS__))
+# if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64) || defined(__WINDOWS__) || defined(__MINGW32__))
 
-#  define __SIHD_WINDOWS__
-#  define __SIHD_PLATFORM__ "windows"
+#  define __SIHD_UNIX__
+#  define __SIHD_LINUX__
+// #  define __SIHD_WINDOWS__
+// #  define __SIHD_PLATFORM__ "windows"
 
 # elif (defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__) \
-        || defined(__CYGWIN__)  || defined(__EMSCRIPTEN__) || defined(__MSYS__))
+        || defined(__CYGWIN__)  || defined(__EMSCRIPTEN__))
 
 #  define __SIHD_UNIX__
 #  define __SIHD_LINUX__
