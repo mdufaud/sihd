18df2bb599e61376d4ed34798c4c927a0734be62
mdufaud <maxence_dufaud@hotmail.fr>
2024-12-02 01:45:09 +0100

    adding is_root for windows

--- util/src/os.cpp
+++ util/src/os.cpp
@@ -167,8 +167,27 @@ bool getsockopt(int socket, int level, int optname, void *optval, socklen_t *opt
 bool is_root()
 {
 #if defined(__SIHD_WINDOWS__)
-# pragma message("TODO os::is_root")
-    return false;
+    BOOL is_admin = FALSE;
+    DWORD dwError = ERROR_SUCCESS;
+    HANDLE hToken = NULL;
+
+    // Open the process token.
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+    {
+        return false;
+    }
+
+    // Retrieve the token elevation information.
+    TOKEN_ELEVATION elevation;
+    DWORD dwSize;
+
+    if (!GetTokenInformation(hToken, TokenElevation, &elevation, sizeof(elevation), &dwSize))
+    {
+        CloseHandle(hToken);
+        return false;
+    }
+
+    return elevation.TokenIsElevated;
 #else
     return getuid() == 0;
 #endif
